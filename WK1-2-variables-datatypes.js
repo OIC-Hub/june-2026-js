27742b26a10f8a2287562d81df5be01be43e7563
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-06-17 10:20:50 +0100

    added class files on variables and datatypes

---
+++ WK1-2-variables-datatypes.js
@@ -0,0 +1,52 @@
+var firstName = "Oga Niyi";
+console.log(firstName);
+
+firstName = "Tolu";
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = 23;
+console.log(firstName);
+console.log(typeof firstName);
+
+let age = 11;
+console.log(age);
+
+age = 15;
+
+const pi = 3.142;
+console.log(pi);
+
+{
+    var firstName = "Sayo";
+    console.log(firstName);
+    let age = 12;
+    console.log(age);
+    const pi = 23;
+    console.log(pi);
+    
+    
+}
+
+console.log(firstName);
+console.log(age);
+console.log(pi);
+
+let sentence1 = "Ade's father is going to school";
+let sentence2 = 'Oga Niyi is very "nice" and good.';
+let sentence3 = `Oga Niyi's wife is "nice" and good.`;
+
+let newage = 23;
+let sentence4 = "I am " + newage + " years old";
+console.log(sentence4);
+
+let sentence5 = `I am ${newage} years old`;
+console.log(sentence5);
+
+
+
+
+
+
+
+
