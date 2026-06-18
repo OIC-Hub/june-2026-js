ac12be532ac0d4f6f4b7104e14453395a52299cb
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-06-18 11:04:14 +0100

    added class files on operators

---
+++ WK1-3-operators.js
@@ -0,0 +1,82 @@
+let a = 50;
+let b = 10;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(2 ** 3);
+console.log(a % b);
+console.log(9 % 2);
+console.log(19 % 7);
+
+let c = 9;
+console.log(c++);
+console.log(c);
+
+let d = 4;
+console.log(++d);
+console.log(d);
+
+let e = 8;
+console.log(e--);
+console.log(e);
+
+let f = 3;
+console.log(--f);
+console.log(f);
+
+let g = 10;
+g += 7;
+console.log(g);
+
+let h = 12;
+h -= 9;
+console.log(h);
+
+let i = 12;
+i *= 2;
+console.log(i);
+
+let j = 12;
+j /= 2;
+console.log(j);
+
+let k = 12;
+k **= 2;
+console.log(k);
+
+let l = 12;
+l %= 2;
+console.log(l);
+
+console.log(5 > 3);
+console.log(-6 > -1);
+console.log(5 < 2);
+console.log(-4 < -2);
+console.log(7 >= 7);
+console.log(7 >= 9);
+console.log(-8 <= 8);
+console.log(9 <= 9);
+console.log(3 == 8);
+console.log(9 == 9);
+console.log(3 == "3");
+console.log(3 === "3");
+console.log(9 === 8);
+
+console.log(3 != 8);
+console.log(9 != 9);
+console.log(3 != "3");
+console.log(3 !== "3");
+console.log(9 !== 8);
+
+console.log(5 > 8 || 2 < 9);
+console.log(6 < 9 && 2 > 9 && 3 < 9);
+console.log(6 < 9 && !(2 > 9) && 3 < 9);
+
+let age = 120;
+let sentence = age >= 18 ? "adult" : "minor";
+console.log(sentence);
+
+
+
+
