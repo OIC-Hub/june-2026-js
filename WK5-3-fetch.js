66418fc915db274116a0d143ea63c43530943a4b
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-07-20 10:37:43 +0100

    update

--- WK5-3-fetch.js
+++ WK5-3-fetch.js
@@ -9,15 +9,35 @@
 // console.log(data)
 
 
-const url = "https://jsonplaceholder.typicode.com/users"
-async function getData(){
-    try {
-       const response =  await fetch(url) 
-        const data = await response.json();
-        console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
+// const url = "https://jsonplaceholder.typicode.com/users"
+// async function getData(){
+//     try {
+//        const response =  await fetch(url) 
+//         const data = await response.json();
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// getData();
+
+
+const api = "https://dummyjson.com/products"
+
+async function fetchData(){
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log(data)
+
+    data.products.forEach(products => {
+        console.log(`Products ${products.id} - ${products.title}`)
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-getData();
+fetchData();
